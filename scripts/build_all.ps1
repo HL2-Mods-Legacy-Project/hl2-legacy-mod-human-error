a13faa5f9b28ebb34c3d1e9dbf72919481adb311
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:57 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Human_Error_1.0.3" `
+  -ModName "Human Error 1.0.3" `
+  -ModFolder "Human_Error" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/human-error/Human-Error-1.0.2+patch-1.0.3.zip" `
+  -ModBaseFilesUrlHash "17f2314f5f24bb3bbef4792b80b7c4bfd5375900d371ad6c185ae5c0922a55b1" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
